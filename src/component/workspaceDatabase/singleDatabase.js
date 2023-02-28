1118d332544e58a61015ec5d87b55c2601e0c5fa
Chanchal Gupta <mvsr1303@gmail.com>
2023-02-28 17:31:54 +0530

    rename db

--- src/component/workspaceDatabase/singleDatabase.js
+++ src/component/workspaceDatabase/singleDatabase.js
@@ -12,9 +12,7 @@ export default function SingleDatabase(props) {
   const [name, setName] = useState(false)
   const [dbname,setDbname ] = useState()
   // const [open, setOpen] = useState(false);
-  const handleOpen = () => {
-       setName(false);
-  }
+  
   const renameDatabase = async (orgId,id,name) =>{
    
 
@@ -43,7 +41,6 @@ const deletDatabases = async(dbId) => {
             { name?
             (<>
                   <TextField
-                  onBlur={handleOpen}
                    autoFocus sx={{ width: 120,fontWeight: 'bold' }} defaultValue={props.db.name} value ={ dbname} 
                    onChange={(e) => {
                     e.preventDefault();
