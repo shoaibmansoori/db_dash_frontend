02544f38fa91ecffaf8f24a4028a5b7623c49480
Deepak solanki <deepak.solanki2335@gmail.com>
2023-03-06 16:27:45 +0530

    create AuthKey

--- src/pages/authKeyPage.js
+++ src/pages/authKeyPage.js
@@ -3,6 +3,8 @@ import AuthKey from "../component/authKeyComponents/authKeyTable";
 import { Box, Button } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import AuthKeyHeader from "../component/authKeyComponents/authKeyHeader";
+import { Link } from "react-router-dom";
+
 
 
 export default function AuthKeyPage() {
@@ -13,9 +15,11 @@ export default function AuthKeyPage() {
      </Box>
 
       <Box sx={{ display: "flex", justifyContent: "end", m: 1 }}>
+        <Link to ='/authKeyCreate' style={{textDecoration:'none'}} >
         <Button variant="contained" startIcon={<AddIcon />}>
           Create Authkey
         </Button>
+        </Link>
       </Box>
 
       <Box sx={{ mt: 2 }}>
---
+++ src/pages/hello.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import AuthKeyHeader from '../component/authKeyComponents/authKeyHeader'
+import Box from '@mui/material/Box';
+
+
+export default function Hello() {
+  return (
+    <>
+    <Box>
+        <AuthKeyHeader/>
+    </Box>
+    <Box>
+
+    </Box>
+    </>
+  )
+}
