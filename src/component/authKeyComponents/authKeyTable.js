f5293b09cb078b69f07e610e3724db34239e1659
hariomm22 <hariommahajan22.com>
2023-03-08 23:56:12 +0530

    unto delete AuthKey and display AuthKey done

--- src/component/authKeyComponents/authKeyTable.js
+++ src/component/authKeyComponents/authKeyTable.js
@@ -12,7 +12,7 @@ import {
 } from "@mui/material";
 
 import { PropTypes } from 'prop-types';
-import { getAuthkey } from "../../api/authkeyApi";
+import { getAuthkey,deleteAuthkey} from "../../api/authkeyApi";
 export default function AuthKey(props) {
 
   const adminId = localStorage.getItem("userid");
@@ -28,7 +28,12 @@ export default function AuthKey(props) {
   async function getAuthkeyFun(){
   const data = await getAuthkey(props.dbId,adminId) 
   setAuthKeys(data?.data?.data)
-  console.log("idris",Object.keys(authKeys))
+  }
+
+  async function deleteAuthkeyFun(authKey){
+    const data = await deleteAuthkey(props.dbId,adminId,authKey)
+    console.log("Delete Auth Key Data : ",data);
+    return data;
   }
   
   console.log("AuthKeys : ",authKeys);
@@ -69,8 +74,9 @@ export default function AuthKey(props) {
                   <TableCell>{authKeys[keys].scope}</TableCell>
                   <TableCell>{authKeys[keys].createBy}</TableCell>
                   <TableCell>{authKeys[keys].createDate}</TableCell>
-                  <TableCell>      <Dropdown first={"Edit"} second={"Delete"} third={"Show AuthKey"} />
-</TableCell>
+                  <TableCell>     
+                     <Dropdown first={"Edit"} second={"Delete"} third={"Show AuthKey"} title={keys} deleteFunction={deleteAuthkeyFun}/>
+                  </TableCell>
             
                 </TableRow>
               ))}
--- src/component/dropdown.js
+++ src/component/dropdown.js
@@ -1,92 +1,97 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Typography, Menu, MenuItem, Tooltip, IconButton } from '@mui/material'
+import {
+  Typography,
+  Menu,
+  MenuItem,
+  Tooltip,
+  IconButton,
+} from '@mui/material';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import AlertPopup from './alertPopup';
-//import { deleteDb } from '../../api/dbApi'
+import DeleteAuthKeyPopup from './authKeyComponents/authKeyTablePopup/deleteAuthkeyPopup';
 
 export default function Dropdown(props) {
-    
-    const [anchorElUser, setAnchorElUser] = useState(null);
-    const [open, setOpen] = useState(false);
-    const handleOpenUserMenu = (event) => {
-      setAnchorElUser(event.currentTarget);
-    };
-    const handleCloseUserMenu = (e) => {
-      e.stopPropagation();
-      setAnchorElUser(null);
-    };
-    const handleClickOpen = () => {
-    
-      setOpen(true);
-    };
+  const [anchorElUser, setAnchorElUser] = useState(null);
+  const [open, setOpen] = useState(false);
 
-     
-    
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = (event) => {
+    event.stopPropagation();
+    setAnchorElUser(null);
+  };
+
+  // const handleClickOpen = () => {
+  //   setOpen(true);
+  // };
+
   return (
+    <>
+      <Tooltip>
+        <IconButton
+          onClick={(event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            handleOpenUserMenu(event);
+          }}
+        >
+          <MoreHorizIcon />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        sx={{ mt: '45px' }}
+        id="menu-appbar"
+        anchorEl={anchorElUser}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        open={Boolean(anchorElUser)}
+        onClose={handleCloseUserMenu}
+      >
+        <MenuItem onClick={handleCloseUserMenu}>
+          <Typography textAlign="center">{props?.first}</Typography>
+        </MenuItem>
 
-    
-    <>  
-        <Tooltip>
-            <IconButton onClick={(e)=>{
-              e.preventDefault();
-              e.stopPropagation();
-              handleOpenUserMenu(e)}}>
-              <MoreHorizIcon />
-            </IconButton>
-          </Tooltip>
-          <Menu
-            sx={{ mt: '45px' }}
-            id="menu-appbar"
-            anchorEl={anchorElUser}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
-            }}
-            open={Boolean(anchorElUser)}
-            onClose={handleCloseUserMenu}
-          >
-            <MenuItem onClick={handleCloseUserMenu}>
-              <Typography textAlign="center" onClick={(e) =>{e.preventDefault();
-              e.stopPropagation(); props?.setName (true)}}>{props?.first}</Typography>
-            </MenuItem>
+        <MenuItem
+          onClick={(event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            handleCloseUserMenu(event);
+            setOpen(true);
+          }}
+        >
+          <Typography textAlign="center">{props?.second}</Typography>
+        </MenuItem>
 
-            <MenuItem onClick={(e)=>{e.preventDefault();
-              e.stopPropagation();handleCloseUserMenu(e);setOpen(true)}}>
-              <Typography  onClick={handleClickOpen} textAlign="center" >{props?.second}</Typography>
-            </MenuItem>
-
-            <MenuItem>
-              <Typography  onClick={handleClickOpen} textAlign="center" >{props?.third}</Typography>
-            </MenuItem>
-            <AlertPopup open={open} setOpen ={setOpen} title={props?.title } deleteFunction={props?.deleteFunction}  /> 
-          </Menu>
+        <MenuItem onClick={(event)=>{handleCloseUserMenu(event);setOpen(true)}}>
+          <Typography textAlign="center">{props?.third}</Typography>
+        </MenuItem>
+        <DeleteAuthKeyPopup
+          open={open}
+          setOpen={setOpen}
+          title={props?.title}
+          deleteFunction={props?.deleteFunction}
+        />
+      </Menu>
     </>
-  )
+  );
 }
+
 Dropdown.propTypes = {
   first: PropTypes.string,
   second: PropTypes.string,
   third: PropTypes.string,
-  setName: PropTypes.func,
   title: PropTypes.string,
-  deleteFunction : PropTypes.func
-  
-    
-  
+  deleteFunction: PropTypes.func,
 };
-
-
-
-
-
-
-
 
 
 
